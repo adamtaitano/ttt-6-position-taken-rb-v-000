b0828c61c8d9116550a629084dc17938b11b61c0
Adam Taitano <adam.taitano@gmail.com>
2018-06-22 18:37:01 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " "
+  if board[index] == " " || "" || nil
+    false
   else
+    true
   end
-  
-  def is_empty?
-  end
-  
   
 end
